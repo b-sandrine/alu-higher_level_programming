64c52fcd3f2fcfd7485a7d2de903b6f7a30c93e8
b-sandrine <sanrdushimimana@gmail.com>
2025-03-04 12:19:02 -0800

    Added whitespace aroung =

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     try:
         count = 0
-        printedCount=0
+        printedCount = 0
         for count in range(x):
             try:
                 print("{:d}".format(my_list[count]), end="")
