527dceac61123e70d63cdf3c063f119584eb148c
b-sandrine <sanrdushimimana@gmail.com>
2025-03-04 12:42:43 -0800

    Task 5 which is about dividing lists

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for i in range(list_length):
+        try:
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                raise IndexError
+            if not isinstance(my_list_1[i], (int, float)) or not isinstance(my_list_2[i], (int, float)):
+                raise TypeError
+            division_result = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("wrong type")
+            division_result = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            division_result = 0
+        except IndexError:
+            print("out of range")
+            division_result = 0
+        finally:
+            result.append(division_result)
+    return result
