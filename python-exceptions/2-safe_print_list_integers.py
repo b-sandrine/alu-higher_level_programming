c5f9a6206f75bfbafff822a4c8d9bafda8112c42
b-sandrine <sanrdushimimana@gmail.com>
2025-03-04 12:17:42 -0800

    Updated to return printedCount

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,13 +4,15 @@
 def safe_print_list_integers(my_list=[], x=0):
     try:
         count = 0
+        printedCount=0
         for count in range(x):
             try:
                 print("{:d}".format(my_list[count]), end="")
                 count += 1
+                printedCount += 1
             except (TypeError, ValueError):
                 continue
     except(IndexError):
         raise
     print()
-    return count
+    return printedCount
