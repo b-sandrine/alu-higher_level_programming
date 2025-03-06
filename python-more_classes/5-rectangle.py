5a11bf344cbdd52bfa5c213e092769791f1a2694
b-sandrine <sanrdushimimana@gmail.com>
2025-03-06 01:42:16 -0800

    Added a method for pringing when an instance is deleted

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" Thhis module contains class called rectangel """
+
+
+class Rectangle:
+    """ Defining a class Rectangle with 2 attributes height and width"""
+
+    def __init__(self, width=0, height=0):
+        """ Instantitiating the attributes """
+        self.width = width
+        self.height = height
+
+    # Getters and setter for the 2 attributes
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    # Area of rectangle
+    def area(self):
+        return self.width * self.height
+
+    # Perimiter of rectangle
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    # __str__ function to print the string representation of the rectange
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        return "\n".join("#" * self.width for _ in range(self.height))
+
+    # __repr__ function to  should return a string representation
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    # Detecting if an instance is deleted
+    def __del__(self):
+        print("Bye rectangle ...")
