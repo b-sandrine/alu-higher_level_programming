6d0271088e3ad8e9e43bb2f7ddd96d268495b9ad
b-sandrine <sanrdushimimana@gmail.com>
2025-03-06 01:20:29 -0800

    fixed the perimeter function

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -41,7 +41,6 @@ class Rectangle:
 
     # Perimiter of rectangle
     def perimeter(self):
-        result = 0
-        if self.width != 0 or self.height != 0:
-            result = 2 * (self.width + self.height)
-        return result
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
