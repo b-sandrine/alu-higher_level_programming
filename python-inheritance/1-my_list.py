56e4558cfed8e2018e0a4fea17a49c80d8a4dc14
b-sandrine <sanrdushimimana@gmail.com>
2025-03-07 10:48:57 -0800

    Task 2

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""A subclass of list that includes a method to print a sorted version of the list."""
+'''This file creates a class that inherits from List builtin functions '''
 
 
 class MyList(list):
-    """A subclass of list that includes a method to print a sorted version of the list."""
+    '''This Class inherits the built in function list'''
 
     def print_sorted(self):
-        """Prints the list in ascending order without modifying the original list."""
-        print(sorted(self))
+        '''Prints the list in a sorted order  '''
+        sorted_list = self[:]
+        sorted_list.sort()
+        print("{}".format(sorted_list))
