f03da3ef66cc8cc82e7521262d302304de9ff1ca
b-sandrine <sanrdushimimana@gmail.com>
2025-03-07 11:18:24 -0800

    updated

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -6,4 +6,4 @@ class BaseGeometry:
     """Raises exception when area is called"""
 
     def area:
-        raise NameError("area() is not implemented")
+        raise Exception("area() is not implemented")
