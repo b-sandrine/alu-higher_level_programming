32c2e947afc4a320dd241d662b9503581e4c7a41
b-sandrine <sanrdushimimana@gmail.com>
2025-03-07 10:57:56 -0800

    Task 3

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+""" This module contains a function whcih checks"""
+
+
+def is_same_class(obj, a_class):
+    """Checking if object is type class"""
+
+    return type(obj) is a_class
