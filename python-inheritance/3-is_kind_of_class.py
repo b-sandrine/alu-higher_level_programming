2a13d92bc0035ade99c3097c28869f89d99b9e7e
b-sandrine <sanrdushimimana@gmail.com>
2025-03-07 11:04:21 -0800

    Task 4

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Checks if obj is instance of class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ tHE CODE TO Check the exact thing"""
+
+    return isinstance(obj, a_class)
